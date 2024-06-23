a4a4a366375447a3285c5148d1c42e8bea657060
Jessica-Bastos <jessicambastoss@gmail.com>
2024-06-23 17:25:30 -0300

    Desafio criado para a conclusão do módulo de Funções do curso de Lógica e Programação da DIO.

---
+++ src/index.js
@@ -0,0 +1,37 @@
+let resultado = subtração (125,37)
+
+
+function subtração(numA,numB) {
+    let subtração = numA - numB
+    return subtração
+}
+
+console.log("Saldo de vitórias do herói: " + resultado)
+
+if(resultado < 10) {
+    console.log("O Herói tem um saldo de " + resultado + " e está classificado no nível de Ferro")
+}
+
+else if(resultado >= 11 && resultado <= 20) {
+    console.log("O Herói tem um saldo de " + resultado + " e está classificado no nível de Bronze")
+}
+
+else if(resultado >= 21 && resultado <= 50) {
+    console.log("O Herói tem um saldo de " + resultado + " e está classificado no nível de Prata")
+}
+
+else if(resultado >= 51 && resultado <= 80) {
+    console.log("O Herói tem um saldo de " + resultado + " e está classificado no nível de Ouro")
+}
+
+else if(resultado >= 81 && resultado <= 90) {
+    console.log("O Herói tem um saldo de " + resultado + " e está classificado no nível de Diamante")
+}
+
+else if(resultado >= 91 && resultado <= 100) {
+    console.log("O Herói tem um saldo de " + resultado + " e está classificado no nível de Lendário")
+}
+
+else {
+    console.log("O Herói tem um saldo de " + resultado + " e está classificado no nível de Imortal")}
+
